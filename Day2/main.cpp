42e3e73a5799a7c877bd5325dcf6c3647307861d
Kenneth Cheong <128382082+kenneth-khc@users.noreply.github.com>
2024-12-08 17:24:28 +0000

    Complete Day2

--- Day2/main.cpp
+++ Day2/main.cpp
@@ -5,57 +5,14 @@
 #include <vector>
 #include <algorithm>
 
-std::vector<int>	vectorize(std::string& report)
-{
-	std::stringstream	ss{report};
-	std::vector<int>	v;
+static bool
+is_safe(std::string& report);
 
-	std::string level;
-	ss >> level;
-	while (ss)
-	{
-		int	num = std::stoi(level);
-		v.push_back(num);
-		ss >> level;
-	}
-	return (v);
-}
+static std::vector<int>
+vectorize(std::string& report);
 
-bool    is_safe(std::string& report)
-{
-	std::vector<int>	v = vectorize(report);
-
-	auto	begin = v.begin();
-	auto	it = begin + 1;
-	auto	end = v.end();
-	int		corrected{0};
-
-	std::adjacent_difference(begin, end, begin);
-
-	for (auto left = it, right = it + 1; left != end; ++left, ++right)
-	{
-		int&	lhs = *left;
-		int&	rhs = *right;
-		
-		if (abs(lhs) == 0 || abs(lhs) > 3)
-		{
-			++corrected;
-		}
-		if (!((lhs < 0) == (rhs < 0)))
-		{
-			++corrected;
-		}
-	}
-	
-	if (corrected <= 1)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
+static bool
+dampen_problem(std::vector<int>& report);
 
 int main()
 {
@@ -73,4 +30,64 @@ int main()
         }
     }
 	std::cout << "Safe count: " << safe_count << std::endl;
+}
+
+static bool
+is_safe(std::string& report)
+{
+	std::vector<int>	v = vectorize(report);
+	std::vector<int>	diff(v.size());
+
+	std::adjacent_difference(v.begin(), v.end(), diff.begin());
+	diff.erase(diff.begin());
+
+	bool	all_positive = std::all_of(diff.begin(), diff.end(), [](int x){return x > 0;});
+	bool	all_negative = std::all_of(diff.begin(), diff.end(), [](int x){return x < 0;});
+	bool	steps_ok = std::all_of(diff.begin(), diff.end(), [](int x){return abs(x) > 0 && abs(x) < 4;});
+
+	if (steps_ok && (all_positive || all_negative))
+	{
+		return true;
+	}
+	else
+	{
+		return dampen_problem(v);
+	}
+}
+
+static std::vector<int>
+vectorize(std::string& report)
+{
+	std::stringstream	ss{report};
+	std::vector<int>	v;
+
+	std::string level;
+	ss >> level;
+	while (ss)
+	{
+		int	num = std::stoi(level);
+		v.push_back(num);
+		ss >> level;
+	}
+	return (v);
+}
+
+static bool
+dampen_problem(std::vector<int>& report)
+{
+	for (size_t i = 0; i < report.size(); ++i)
+	{
+		std::vector<int>	test{report};
+		test.erase(test.begin() + i);
+		std::adjacent_difference(test.begin(), test.end(), test.begin());
+		test.erase(test.begin());
+		bool	all_positive = std::all_of(test.begin(), test.end(), [](int x){return x > 0;});
+		bool	all_negative = std::all_of(test.begin(), test.end(), [](int x){return x < 0;});
+		bool	steps_ok = std::all_of(test.begin(), test.end(), [](int x){return abs(x) > 0 && abs(x) < 4;});
+		if (steps_ok && (all_positive || all_negative))
+		{
+			return true;
+		}
+	}
+	return false;
 }
